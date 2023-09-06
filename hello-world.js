9ac1f229b0040bb07408c762a61f45614067cb9f
macklark <ganesh.kolavennu@gmail.com>
2023-09-06 21:33:58 +0530

    hello-world

---
+++ hello-world.js
@@ -0,0 +1,11 @@
+console.log('Hello, world !');
+
+// To execute this first line of code you can either use a browser or Javascript runtime such as node js.
+
+// 1. Browser -> We can use the V8 Javascript Engine of chrome browser. Just download the Live Server extension from VSCode https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer, Enable the extension and you are good to go.
+
+// 2. Node js -> Download Node js (i.e. Javascript runtime) on your machine from here https://nodejs.org. Now you can execute Javascript on your machine without a browser.
+
+// For this example just run this command in your terminal
+
+// node hello-world.js
