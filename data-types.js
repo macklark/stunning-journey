b34a32e0c0391c0ff253f953d867caa06a73e99e
macklark <ganesh.kolavennu@gmail.com>
2023-09-07 21:19:19 +0530

    data-types

--- data-types.js
+++ data-types.js
@@ -44,7 +44,86 @@ console.log(userOneAgeWithMonths + userTwoAgeWithMonths); // You would expect it
 
 // This is because of rounding errors caused in some cases, so being aware of this kind of errors is better.
 
-// A small hint -> If we want to represent a very big value or store it in a variable we can do this.
+// A small hint -> If we want to represent a very big number or store it in a variable we can do this.
 
 var aVeryBigNumber = 100000;
 var betterWayOfRepresentingAVeryBigNumber = 1_00_000;
+
+// String Type
+
+// string of charecters is essentially a text.
+
+// string is a datatype where we can store any kind of value in between quotes for ex. 'hello', '10', ')89tgb', etc...
+
+var userOneName = 'Jane';
+var userOneAge = '20';
+
+console.log(userOneName, userOneAge); // logs out Jane 20
+
+console.log(typeof userOneName, typeof userOneAge) // You might think that it will return string number. But it does not it will return string string because here userOneAge is storing a string value in between quoutes.
+
+// There is another way to define string;
+
+var userOneCity = "Visakhapatnam";
+// We can define strings using '' or ""
+
+// string concatenation
+// We can use the '+' operator to concat two or more strings.
+
+var greetings = 'Hello_';
+
+console.log(greetings + userOneName + '!'); // logs out Hello_Jane!
+
+// Boolean Type
+
+// boolean is a kind of datatype where it answers a question either with yes / no or 0 / 1 or true / false.
+
+// boolean datatype is either true or false keywords.
+
+var isUserSubscribed = true;
+
+console.log(isUserSubscribed, typeof isUserSubscribed); // logs out true boolean.
+
+var isAnotherUserSubscribed = false;
+console.log(isAnotherUserSubscribed, typeof isAnotherUserSubscribed); // logs out false boolean.
+
+// With booleans we cannot use general arithematic operators because it does not make sense when we say
+
+console.log(isUserSubscribed + isAnotherUserSubscribed); // logs out 1 this is unexpected
+
+// Anyways there are special kind of operators called logical operators which apply to boolean datatypes.
+
+// (&&) Ampersand is 'and' operator which means both the values are supposed to be true.
+
+console.log(isUserSubscribed && isAnotherUserSubscribed); // logs out false.
+
+// (||) doublepipe is 'or' operator which means either of the value is supposed to be true.
+
+console.log(isUserSubscribed || isAnotherUserSubscribed); // logs out true.
+
+// There is a concept called boolean inversion in javascript, basically it is inverting the boolean value to the other boolean value.
+
+console.log(!isUserSubscribed); // logs out false
+
+// double inversion
+console.log(!!isUserSubscribed); // logs out true
+
+// Null and Undefined
+
+// null and undefined are a kind of datatypes where we say that we do not have the value.
+
+var doNotHaveAValue;
+
+console.log(doNotHaveAValue); // logs out undefined
+
+// We can explicitly declare undefined to a variable.
+doNotHaveAValue = undefined;
+
+console.log(doNotHaveAValue);
+console.log(typeof doNotHaveAValue);
+
+// undefined is when we do not have a value, while null is used to explicitly say that we do not have the specific value.
+
+var explicitlySayDoNotHaveAValue = null;
+
+console.log(explicitlySayDoNotHaveAValue); // log out null.
